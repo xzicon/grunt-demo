d7628e525f1a9f514b59e01f30f14052af09f681
xuzheng <1213157022@qq.com>
2020-03-18 02:52:17 +0800

    x

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,37 @@
 module.exports = function (grunt) {
+rts = function (grunt) {
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseWhitespace: true
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/second.css': 'second.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/second.js': 'second.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask("release", ['copy', 'htmlmin','cssmin', 'uglify']);
+};
   grunt.initConfig({
     htmlmin: {
       options: {
@@ -14,38 +47,4 @@ module.exports = function (grunt) {
   }
         
       
-    },
-  cssmin: {
-              'dist/second.css': 'second.css'
-            
-      
-  },
-  uglify: {
-    release:{
-      files: {
-                          'dist/second.js': 'second.js'
-                                                        
-                                  
-      }
-                                
-                 
-    }
-                            
-              
-  }
-      
-    
-  });
 
-      grunt.loadNpmTasks('grunt-contrib-htmlmin');
-
-            grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-                    grunt.loadNpmTasks('grunt-contrib-uglify');
-                              grunt.loadNpmTasks("grunt-contrib-copy");
-
-                                          grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
-                                                        grunt.registerTask("release", ['copy', 'htmlmin','cssmin', 'uglify']);
-
-
-};
